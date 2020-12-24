f40b7e67aaff52987f1c06a907b8f86dc7da5123
wuwei <YG2920@baosight.com>
2020-12-24 18:08:37 +0800

    基于tinymce 的page builer插件开发

--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -1,11 +1,26 @@
-declare const tinymce: any;
+import { either } from '@ephox/agar/lib/main/ts/ephox/agar/assertions/ApproxStructures';
+import * as tinyMCE from '../../../node_modules/tinymce/tinymce';
+import tinymce from '../../../node_modules/tinymce/tinymce';
+// declare const tinymce: any;
 
-const setup = (editor, url) => {
+
+
+const setup = (editor: tinyMCE.Editor, url) => {
   editor.ui.registry.addButton('page-bulider', {
-    text: 'page-bulider button',
+    text: 'page-builder button',
     onAction: () => {
-      // tslint:disable-next-line:no-console
-      editor.setContent('<p>content added from page-bulider</p>');
+      editor.windowManager.open({
+        title: 'test',
+        body: [
+          
+        ],
+        onSubmit: e => {
+          editor.insertContent('Title: ' + e.getData.name);
+        }
+
+      });
+
+      editor.setContent('<p>haha content added from page-bulider</p>');
     }
   });
 };
